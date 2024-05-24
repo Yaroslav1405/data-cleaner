122b3a28e76974de8801151228505c49263ae0ba
Yaroslav Hinda <yaroslav.hinda@gmail.com>
2024-05-24 12:25:42 +0300

    More Functionality

--- data-cleaner.py
+++ data-cleaner.py
@@ -1,10 +1,12 @@
 import os
 import pandas as pd
-import numpy as np
+from PIL import Image
 import tkinter as tk
 import customtkinter as ctk
+
 ctk.set_appearance_mode('System')
 ctk.set_default_color_theme('green')
+
 class App(ctk.CTk):
     width = 900
     height = 600
@@ -14,118 +16,100 @@ class App(ctk.CTk):
         
         self.title('Data Cleaner')
         self.geometry(f"{self.width}x{self.height}")
-        self.resizable(False, False)
-
+#        self.resizable(False, False)
 
         self.df = None
-        self.selected_column = None
+        self.column = None
         self.first_rows = None
-
-        # Create Main Frame
+        self.tree = None
+        self.insights_text = None
+        
+        # Load and Create Background Image
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        self.bg_image = ctk.CTkImage(Image.open(current_path + "/images/bg_img_main.jpeg"),
+                                     size=(self.width, self.height))
+        self.bg_image_label = ctk.CTkLabel(self, image=self.bg_image)
+        self.bg_image_label.grid(row=0, column=0)
+        
+    # Main Frame
         self.main_frame = ctk.CTkFrame(self, corner_radius = 0)
         self.main_frame.grid(row=0, column = 0, sticky='ns')
-@@ -27,10 +30,36 @@ def __init__(self, *args, **kwargs):
-        self.select_file_label.grid(row = 0, column = 0, padx = 30, pady = (150,15))
+        
+        # File Selection Part 
+        self.select_file_label = ctk.CTkLabel(self.main_frame, text="Select File to open",
+                                              font=ctk.CTkFont(size = 18, weight= 'bold'))
+        self.select_file_label.grid(row = 0, column = 0, padx = 50, pady = (150, 15))
         self.select_file_btn = ctk.CTkButton(self.main_frame, text="Select File", command=self.open_file)
-        self.select_file_btn.grid(row = 1, column = 0, padx = 30, pady=(15,15))
-        # Create Data Manipulation frame
-
-
-
-
-
-        # Create Column Selection Frame
-        self.select_cols_frame = ctk.CTkFrame(self, corner_radius = 0)
-        self.select_cols_frame.grid_columnconfigure(0, weight=1)
-        self.select_cols_label = ctk.CTkLabel(self.select_cols_frame, text="Select a column to modify")
-        self.select_cols_label.grid(row=1, column = 0, sticky='ns', pady=10, padx=100)
-        self.column_selection = ctk.CTkComboBox(self.select_cols_frame, values=[], state='readonly')
-        self.column_selection.grid(row=2, column=0, padx=100, pady=10)
-        self.select_cols_btn = ctk.CTkButton(self.select_cols_frame, text="Select Column", command=self.selected_column_event)
-        self.select_cols_btn.grid(row=3, column=0, padx=30, pady=(15,15))
+        self.select_file_btn.grid(row = 1, column = 0, padx = 50, pady=15)
+        
+        # Column Selection Part
+        self.select_cols_label = ctk.CTkLabel(self.main_frame, text="Select a column to modify")
+        self.select_cols_label.grid(row=0, column = 1, sticky='ns', padx=50, pady=(150,15))
+        self.column_selection = ctk.CTkComboBox(self.main_frame, values=[], state='readonly')
+        self.column_selection.grid(row=1, column=1, padx=50, pady=15)
+        self.select_cols_btn = ctk.CTkButton(self.main_frame, text="Select Column", command=self.selected_column_event)
+        self.select_cols_btn.grid(row=2, column=1, padx=30, pady=15)
         self.select_cols_btn.configure(state="disabled")
-        self.back_button = ctk.CTkButton(self.select_cols_frame, text="Back", command=self.back_event, width=200)
-        self.back_button.grid(row=4, column=0, padx=30, pady=(15,15))
-
-        # Create Data Manipulation Frame
+        
+    # Data Manipulation Frame
         self.clean_data_frame = ctk.CTkFrame(self, corner_radius=0)
         self.clean_data_frame.grid_columnconfigure(0, weight=1)
-        self.clean_data_label = ctk.CTkLabel(self.clean_data_frame, text="Clean Data in Column")
-        self.clean_data_label.grid(row=0, column=0, pady=10, padx=100)
-        self.first_rows_label = ctk.CTkLabel(self.clean_data_frame, text=self.first_rows)
-        self.first_rows_label.grid(row=1, column=2)
-        self.clean_data_btn = ctk.CTkButton(self.clean_data_frame, text="Clean Data", command=self.clean_data)
-        self.clean_data_btn.grid(row=2, column=0, padx=100, pady=10)
-        self.back_button_clean = ctk.CTkButton(self.clean_data_frame, text="Back", command=self.back_to_select_cols, width=200)
-        self.back_button_clean.grid(row=3, column=0, padx=30, pady=(15, 15))
-
-        self.tree = None
-
+        
+        # Table with Samples
+        self.tb_label = ctk.CTkLabel(self.clean_data_frame, text= "First 5 samples from column", font=ctk.CTkFont(size=18, weight='bold'))
+        self.tb_label.grid(row = 0, column=0, padx=10)
+        
+        # Column Description
+        self.tb_label = ctk.CTkLabel(self.clean_data_frame, text= "Column Description", font=ctk.CTkFont(size=18, weight='bold'))
+        self.tb_label.grid(row = 0, column=1, padx=10)
+        self.insights = ctk.CTkTextbox(self.clean_data_frame, width=250, height=230, state='disabled')
+        self.insights.grid(row=1, column=1)
+        self.insights.configure(state='disabled')
+        
+        # Cleaning Options
+        self.check_var1, self.check_var2, self.check_var3, self.check_var4 = ctk.StringVar(value='off'), ctk.StringVar(value='off'), ctk.StringVar(value='off'), ctk.StringVar(value='off')
+        # self.check_var2 = ctk.StringVar(value='off')
+        # self.check_var3 = ctk.StringVar(value='off')
+        # self.check_var4 = ctk.StringVar(value='off')
+        self.checkbox_1 = ctk.CTkCheckBox(master=self.clean_data_frame, text="Delete NaN values", variable = self.check_var1, onvalue='on', offvalue='off')
+        self.checkbox_1.grid(row=2, column=0, padx=10, pady=10)
+        self.checkbox_2 = ctk.CTkCheckBox(master=self.clean_data_frame, text="Fill NaN with 0", variable = self.check_var2, onvalue='on', offvalue='off')
+        self.checkbox_2.grid(row=2, column=1, padx=10, pady=10)
+        self.checkbox_3 = ctk.CTkCheckBox(master=self.clean_data_frame, text="Remove duplicates", variable = self.check_var3, onvalue='on', offvalue='off')
+        self.checkbox_3.grid(row=2, column=2, padx=10, pady=10)
+        self.checkbox_4 = ctk.CTkCheckBox(master=self.clean_data_frame, text="Clean ?~()$#@!%*; values", variable = self.check_var4, onvalue='on', offvalue='off')
+        self.checkbox_4.grid(row=2, column=3, padx=10, pady=10)
+        self.clean_btn = ctk.CTkButton(self.clean_data_frame, text='Submit', command = self.clean_event, width=180, height=40)
+        self.clean_btn.grid(row=3, column=1, padx=10, pady=10)
+        self.clean_btn = ctk.CTkButton(self.clean_data_frame, text='Clear', command = self.clear_selection, fg_color='#EE6055')
+        self.clean_btn.grid(row=3, column=0, padx=10, pady=10)
+        self.clean_btn = ctk.CTkButton(self.clean_data_frame, text='Toggle', command = self.toggle_selection, fg_color='#035E7B')
+        self.clean_btn.grid(row=3, column=2, padx=10, pady=10)
+        
+        
+    # Open File Function                      
     def open_file(self):
         global df
-@@ -39,39 +68,57 @@ def open_file(self):
+        file = tk.filedialog.askopenfilename(
+            title='Select a file',
             filetypes=(("CSV files", "*.csv"),)
         )
         if file:
-            df = pd.read_csv(file)
-            self.select_file_label.configure(text=f'Selected directory {file}')
-           # self.select_cols_btn.config(state="normal")
-            #select_cols()
             self.df = pd.read_csv(file)
             self.column_selection.configure(values=self.df.columns.tolist())
             self.select_cols_btn.configure(state="normal")
-            self.next_event()
-
-    def next_event(self):
-        self.main_frame.grid_forget() # remove main frame
-        self.select_cols_frame.grid(row=0, column=0, sticky='nsew', padx=100)
-        self.select_cols_frame.grid(row=0, column=0, sticky='nsew', padx=300, pady=200)
 
 
-
-    def back_event(self):
-        self.select_cols_frame.grid_forget()
-        self.main_frame.grid(row=0, column=0, sticky='ns')
-
-    # def select_cols(self):
-    #     # Destroy the current frame
-    #     for widget in app.winfo_children():
-    #         widget.destroy()
-
+   
+    # Column Selection and Table Creation Function
     def selected_column_event(self):
-        self.column = self.column_selection.get()
-
-        # print("Column selected: ", self.column)
-        # self.first_rows = self.df[f'{self.column}'].head(5)
-        # print(self.first_rows)    
-        self.select_cols_frame.grid_forget()
+        self.column = self.column_selection.get()   
+        self.main_frame.grid_forget()
         self.clean_data_frame.grid(row=0, column=0, sticky='nsew')
-        if self.column:
-            self.clean_data_frame_event()
-
-    #     # Create a new frame with desired content
-    #     new_frame = ctk.CTkFrame(app)
-    #     new_frame.pack(pady=10)
-    #     label = ctk.CTkLabel(new_frame, text="Select a column to modify")
-    #     label.pack(pady=20)
-    #     if 'df' in globals():
-    #         column_options = df.columns.tolist()
-    #         column_selection = ctk.CTkComboBox(new_frame, values=column_options, state='readonly')
-    #         column_selection.pack(pady=10)
-    #         select_cols_btn.configure(state="disabled")
-    #     else:
-    #         tk.messagebox.showinfo("Info", "Please select a CSV file first.")
-    #     select_cols_btn = ctk.CTkButton(new_frame, text="Select Column", command=self.select_cols)
-    #     select_cols_btn.pack(pady=10)
-
-    def clean_data_frame_event(self):
-        self.select_cols_frame.grid_forget()
-        self.clean_data_frame.grid(row=0, column=0, sticky='nsew', padx=150, pady=100)
-
         # Display selected column data in a table
         if self.tree:
             self.tree.destroy()
-
+            
         self.tree = tk.ttk.Treeview(self.clean_data_frame, columns=('Value'), show='headings')
         self.tree.heading('Value', text=self.column)
         self.tree.grid(row=1, column=0, padx=20, pady=20, sticky='nsew')
@@ -133,14 +117,44 @@ class App(ctk.CTk):
         for value in self.df[self.column].head(5):
             self.tree.insert('', tk.END, values=(value,))
 
-    def back_to_select_cols(self):
-        self.clean_data_frame.grid_forget()
-        self.select_cols_frame.grid(row=0, column=0, sticky='nsew')
+        # Display some insights about the data
+        insights_text = (f'{self.df[self.column].describe()} \n'
+                         f'Number of values in the column: {self.df[self.column].count()} \n'
+                         f'Null values in the column: {self.df[self.column].isnull().sum()} \n'
+                         f'Data type: {self.df[self.column].dtype}')
+        self.insights.configure(state='normal')
+        self.insights.delete('1.0', tk.END)
+        self.insights.insert(tk.END, insights_text)
+        self.insights.configure(state='disabled')
 
-    def clean_data(self):
-        # Implement data cleaning logic here
-        pass    
+    # Data Cleaning Function
+    def clean_event(self):
+        if self.check_var1.get() == 'on':
+            self.df[self.column] = self.df[self.column].dropna() # Inplace not the good choice here
+            print('Cleaned successfully')
+        if self.check_var2.get() == 'on':
+            self.df[self.column].fillna(value = 0, inplace=True)
+            print('Filled Successfully')
+        if self.check_var3.get() == 'on':
+            self.df.drop_duplicates(subset=[self.column], inplace=True)
+            print('Duplicates Removed')
+        if self.check_var4.get() == 'on':
+            print("Clean ?~()$#@!%*; values")
 
+    # Clear Checkbox Selection Function 
+    def clear_selection(self):
+        self.checkbox_1.deselect()       
+        self.checkbox_2.deselect()       
+        self.checkbox_3.deselect()
+       
+    # Toggle Checkbox Selection Function
+    def toggle_selection(self):
+        self.checkbox_1.toggle()
+        self.checkbox_2.toggle()
+        self.checkbox_3.toggle()
+        
+# Run App             
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+
